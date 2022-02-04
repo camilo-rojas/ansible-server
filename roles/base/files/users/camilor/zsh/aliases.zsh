ecb5451968c818704ee44c529ce91082efee95b6
Camilo Rojas <camilo.rojas@gmail.com>
2022-02-04 16:46:28 -0500

    fixed zsh oh

--- roles/base/files/users/camilor/zsh/aliases.zsh
+++ roles/base/files/users/camilor/zsh/aliases.zsh
@@ -1,23 +1,12 @@
-# Command aliases
-alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
 alias back='cd $OLDPWD'
 alias c='clear'
 alias cd..='cd ..'
 alias cp='cp -iv'
-alias chmod="chmod -c"
-alias chown="chown -c"
 alias df='df -h -x squashfs -x tmpfs -x devtmpfs'
 alias e="nano"
-alias E="vim -o "
 alias egrep='egrep --colour=auto'
 alias extip='curl icanhazip.com'
 alias grep='grep --color=auto'
-alias l.='ls -lhFa --time-style=long-iso --color=auto'
-alias ll='ls'
-alias ln='ln -iv'
-alias ls=' ls -lhF --color=auto --human-readable --time-style=long-iso --classify'
 alias lsmount='mount |column -t'
 alias mkdir='mkdir -pv'
 alias mv='mv -iv'
@@ -25,22 +14,16 @@ alias ports='netstat -tulanp'
 alias h='history -i 1'
 alias history='history 1'
 alias j='jobs -l'
-alias rm='rm -iv'
+#alias rm='rm -iv'
 alias rmdir='rmdir -v'
-alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -'
+alias speedtest='curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python $'
+alias ssha='eval $(ssh-agent) && ssh-add'
 alias ssha='eval $(ssh-agent) && ssh-add'
 alias svim='sudo vim'
 alias tn='tmux new -s'
 alias watch='watch -d'
 alias weather='curl wttr.in'
 alias wget='wget -c'
-
-if command -v colordiff > /dev/null 2>&1; then
-    alias diff="colordiff -Nuar"
-else
-    alias diff="diff -Nuar"
-fi
-
 ## get top process eating memory
 alias mem5='ps auxf | sort -nr -k 4 | head -5'
 alias mem10='ps auxf | sort -nr -k 4 | head -10'
@@ -61,15 +44,3 @@ alias rm='rm -I --preserve-root'
 alias chown='chown --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chgrp='chgrp --preserve-root'
-
-# Package management
-if [ -f /usr/bin/apt ]; then
-  alias update='sudo apt update'
-  alias upgrade='sudo apt update && sudo apt dist-upgrade && sudo apt autoremove && sudo apt clean'
-  alias install='sudo apt install'
-fi
-if [ -f /usr/bin/pacman ]; then
-  alias update='sudo pacman -Syyy'
-  alias upgrade='sudo pacman -Syu'
-  alias install='sudo pacman -S'
-fi
